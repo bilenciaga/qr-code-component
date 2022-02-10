70913427601410766f099133eb7c895632471a0d
NGUYEN HOAI TRUNG <nghtrung1001@gmail.com>
2022-02-10 17:55:37 +0900

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="min-h-screen bg-slate-500 flex items-center justify-center">
+        <div className="w-80 p-1 rounded-2xl text-center bg-white">
+          <div className="p-2">
+            <img className='rounded-2xl' src="/image-qr-code.png"></img>
+          </div>
+          <div className="py-2">
+            <h1 className="font-medium text-2xl">Improve your front-end skills by building projects</h1>
+            <p className="py-5 text-sm">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+          </div>
+        </div>
+
+      </div>
+  )
 }
 
 export default App;
