65bb005a4e1dba1f1531da64b8169fda21d11daa
NGUYEN HOAI TRUNG <nghtrung1001@gmail.com>
2022-02-10 18:37:57 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     <div className="min-h-screen bg-slate-500 flex items-center justify-center">
         <div className="w-80 p-1 rounded-2xl text-center bg-white">
           <div className="p-2">
-            <img className='rounded-2xl' src="/image-qr-code.png"></img>
+            <img className='rounded-2xl' src='./images/image-qr-code.png'></img>
           </div>
           <div className="py-2">
             <h1 className="font-medium text-2xl">Improve your front-end skills by building projects</h1>
